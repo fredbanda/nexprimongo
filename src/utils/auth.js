6096b76ba9e69bdf443a22059915bc2b8809f016
Fred Banda <97950605+fredbanda@users.noreply.github.com>
2023-10-03 17:43:31 +0200

    Update auth.js
    removed credentials

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -7,8 +7,8 @@ export const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
-      clientId: "795896475408-t249ssdjgbkc52e6jrtilhm7t9f6vheo.apps.googleusercontent.com",
-      clientSecret: "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",
+      clientId: "",
+      clientSecret: "",
     }),
   ],
 };
